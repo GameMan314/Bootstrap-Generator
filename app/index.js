ccc36c5d7254d2da1a366892bad8c529650f8176
GameMan314 <Jman64314@gmail.com>
2013-10-26 15:12:27 -0400

    Added Template

--- app/index.js
+++ app/index.js
@@ -22,26 +22,155 @@ TestGenerator.prototype.askFor = function askFor() {
   // have Yeoman greet the user.
   console.log(this.yeoman);
 
-  var prompts = [{
-    type: 'confirm',
-    name: 'someOption',
-    message: 'Would you like to enable this option?',
-    default: true
-  }];
+  var prompts = [
+      {
+          name: "name",
+          message: "What is the name of your project?"
+      },
+ 
+      {
+          name: "description",
+          message: "Give me a description on what your app is supposed to do",
+          default: "A sample description"
+      },
+ 
+      {
+          name: "keywords",
+          message: "Give me keywords that correspond to the site. (comma-separated)",
+          default: "keyword 1, keyword 2, keyword 3, etc."
+      },
+ 
+      //author and header info
+      {
+          name: "authorName",
+          message: "Who is the creator of this site?",
+          default: "Your Name",
+      },
+ 
+      {
+          name: "authorGitHub",
+          default: "Username",
+          message: "What is your gitHub account?"
+      },
+ 
+      {
+          name: "authorCompanyName",
+          default: "Company Name",
+          message: "(optional) What is your company name?"
+      },
+	  
+      {
+          name: "includeGlyphs",
+		  type: "confirm",
+          default: true,
+          message: "Include glyphs?"
+      },
+	  
+	  {
+		  name: "include404",
+		  type: "confirm",
+		  default: true,
+          message: "Include 404 page?"
+	  },
+	  
+	  {
+		  name: "includeRobotsTxt",
+		  type: "confirm",
+		  default: true,
+          message: "Include Robots.txt?"
+	  },
+	  
+	  {
+		  name: "includeHtaccess",
+		  type: "confirm",
+		  default: true,
+          message: "Include .htaccess file?"
+	  },
+	  
+	  {
+		  name: "includeIcons",
+		  type: "confirm",
+		  default: true,
+          message: "Include Icons?"
+	  }
+	  
+  ];
 
   this.prompt(prompts, function (props) {
-    this.someOption = props.someOption;
+    this.name = props.name;
+	this.description = props.description;
+	this.keywords = props.keywords;
+	this.authorName = props.authorName;
+	this.authorGitHub = props.authorGitHub;
+	this.authorCompanyName = props.authorCompanyName;
+	this.includeGlyphs = props.includeGlyphs;
+	this.include404 = props.include404;
+	this.includeRobotsTxt = props.includeRobotsTxt
+	this.includeHtaccess = props.includeHtaccess
+	this.includeIcons = props.includeIcons
 
     cb();
   }.bind(this));
 };
 
 TestGenerator.prototype.app = function app() {
-  this.mkdir('app');
-  this.mkdir('app/templates');
-
-  this.copy('_package.json', 'package.json');
-  this.copy('_bower.json', 'bower.json');
+  this.template("_index.html", "index.html");
+  
+  //...
+  
+  //css
+  this.mkdir("css");
+  this.copy("css/bootstrap.css");
+  this.copy("css/bootstrap.min.css");
+  this.copy("css/bootstrap-theme.css");
+  this.copy("css/bootstrap-theme.min.css");
+  this.copy("css/main.css");
+  
+  //JavaScript
+  this.mkdir("js");
+  this.copy("js/main.js");
+  this.copy("js/plugins.js");
+  
+  this.mkdir("vendor");
+  this.copy("js/vendor/bootstrap.js");
+  this.copy("js/vendor/bootstrap.min.js");
+  this.copy("js/vendor/jquery-1.10.1.min.js");
+  this.copy("js/vendor/modernizr-2.6.2-respond-1.1.0.min.js");
+  
+  //robots
+  if (this.includeRobotsTxt) {
+	    this.copy("robots.txt");
+   }
+   
+  //htaccess
+  if (this.includeHtaccess){
+	  this.copy(".htaccess");
+   }
+  
+  //icons
+  if (this.includeIcons) {
+	  this.copy("favicon.ico");
+	  this.copy("apple-touch-icon.png");
+	  this.copy("apple-touch-icon-57x57-precomposed.png");
+	  this.copy("apple-touch-icon-72x72-precomposed.png");
+	  this.copy("apple-touch-icon-114x114-precomposed.png");
+	  this.copy("apple-touch-icon-144x144-precomposed.png");
+	  this.copy("apple-touch-icon-precomposed.png");
+   }
+  
+  //Glyphs
+  if (this.includeGlyphs) {
+        this.mkdir("fonts");
+        this.copy("fonts/glyphicons-halflings-regular.eot", "fonts/glyphicons-halflings-regular.eot");
+		this.copy("fonts/glyphicons-halflings-regular.svg", "fonts/glyphicons-halflings-regular.svg");
+		this.copy("fonts/glyphicons-halflings-regular.ttf", "fonts/glyphicons-halflings-regular.ttf");
+		this.copy("fonts/glyphicons-halflings-regular.woff", "fonts/glyphicons-halflings-regular.woff");
+    }
+	
+  //404 Page
+  if (this.includeGlyphs) {
+	    this.copy("404.html");
+   }
 };
 
 TestGenerator.prototype.projectfiles = function projectfiles() {
